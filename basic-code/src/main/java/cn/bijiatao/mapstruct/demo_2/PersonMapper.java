b14b1dc844f277af286d39eceaa438ad28e20c5b
宋洪涛-cic <youremail@example.com>
2025-09-12 10:56:33 +0800

    feat：mapstruct基础代码

---
+++ basic-code/src/main/java/cn/bijiatao/mapstruct/demo_2/PersonMapper.java
@@ -0,0 +1,31 @@
+package cn.bijiatao.mapstruct.demo_2;
+
+import cn.bijiatao.mapstruct.model.Person;
+import cn.bijiatao.mapstruct.model.PersonDTO;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDate;
+
+/**
+ * mapstruct 映射
+ *
+ * @author bijiatao
+ */
+@Mapper(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public interface PersonMapper {
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mappings({
+            @Mapping(source = "birthday", target = "myBirthday", qualifiedByName = "toStringBirthday"),
+            @Mapping(source = "idCard", target = "idNo")
+            // mapstruct也不支持静态变量转换，如果加了下一行代码，会抛异常
+            // @Mapping(target = "description", source = "description"),
+    })
+    PersonDTO toPersonDTO(Person person);
+
+    @Named("toStringBirthday")
+    default String toStringBirthday(LocalDate birthday) {
+        return birthday.toString();
+    }
+}
---
+++ basic-code/src/main/java/cn/bijiatao/mapstruct/demo_2/demo_2.java
@@ -0,0 +1,47 @@
+package cn.bijiatao.mapstruct.demo_2;
+
+import cn.bijiatao.mapstruct.model.Address;
+import cn.bijiatao.mapstruct.model.Person;
+import cn.bijiatao.mapstruct.model.PersonDTO;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * mapstruct 基本代码
+ * @author bijiatao
+ */
+@Slf4j
+public class demo_2 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("张三");
+        person.setAge("18");
+        person.setSex("男");
+        person.setIdCard("110101199001011234");
+        Address address = new Address();
+        address.setProvince("广东省");
+        address.setCity("深圳市");
+        address.setArea("南山区");
+        person.setAddress(address);
+        person.setBirthday(LocalDate.now());
+        List<String> hobbyList = new ArrayList<>();
+        hobbyList.add("看电影");
+        hobbyList.add("听音乐");
+        person.setHobbyList(hobbyList);
+        Person.description = "这是Person描述";
+
+        PersonDTO convertorPerson = PersonMapper.INSTANCE.toPersonDTO(person);
+
+        log.info("source-person: {}", person);
+        log.info("target-personDTO: {}", convertorPerson);
+
+        log.info("----因为对象赋值的是引用，而不是深copy，如下------");
+
+        person.getAddress().setProvince("河北省");
+        log.info("source-person: {}", person);
+        log.info("target-personDTO: {}", convertorPerson);
+    }
+}
